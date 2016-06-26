063f4c116722dcc041a8b0c7d185d24b2053e240
nodejs1208 <ting3528@gmail.com>
2016-06-26 20:10:57 +0900

    .delete(teacher.delete)

--- app/controllers/teacher.server.controller.js
+++ app/controllers/teacher.server.controller.js
@@ -1,13 +1,62 @@
 var Teacher = require('mongoose').model('Teacher');
 
-exports.create = function(req,res,next){
-  var teacher = new Teacher(req.body);
+exports.create = function(req, res, next) {
+    var teacher = new Teacher(req.body);
 
-  teacher.save(function(err){
+    teacher.save(function(err) {
+        if (err) {
+            return next(err);
+        } else {
+            res.json(teacher);
+        }
+    });
+};
+
+exports.list = function(req, res, next) {
+    Teacher.find({}, function(err, teacher) {
+
+        if (err) {
+            return next(err);
+        } else {
+            res.json(teacher);
+        }
+    });
+};
+
+exports.read = function(req, res) {
+    res.json(req.teacher);
+};
+
+exports.teacherByID = function(req, res, next, id) {
+
+    Teacher.findOne({
+        _id: id
+    }, function(err, teacher) {
+        if (err) {
+            return next(err);
+        } else {
+            req.teacher = teacher;
+            next();
+        }
+    });
+};
+
+exports.update = function(req, res, next) {
+    Teacher.findByIdAndUpdate(req.teacher.id, req.body, function(err, teacher) {
+        if (err) {
+            return next(err);
+        } else {
+            res.json(teacher);
+        }
+    });
+};
+
+exports.delete=function (req,res,next) {
+  req.teacher.remove(function(err){
     if(err){
       return next(err);
     }else{
-      res.json(teacher);
+      res.json(req.teacher);
     }
   });
 };
--- app/routes/teacher.server.routes.js
+++ app/routes/teacher.server.routes.js
@@ -2,5 +2,15 @@ var teacher = require('../../app/controllers/teacher.server.controller');
 
 module.exports = function(app){
 
-  app.route('/teacher').post(teacher.create);
-}
+  app.route('/teacher')
+    .post(teacher.create)
+    .get(teacher.list)
+
+  app.route('/teacher/:teacherId')
+     .get(teacher.read)
+     .put(teacher.update)
+     .delete(teacher.delete);
+
+  app.param('teacherId',teacher.teacherByID);
+
+};
